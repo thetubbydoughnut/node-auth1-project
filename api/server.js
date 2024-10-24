c4f8f9ec62fefaa5074f223611f9aeb39574c1ef
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-24 14:07:45 -0500

    wip but its broken rn

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -19,6 +21,25 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep it secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+}));
 server.use(cors());
 
 server.get("/", (req, res) => {
