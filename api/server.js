4511eff538c5ec356725f18e9bee94c3fc19cb27
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-24 14:15:35 -0500

    store didnt work so its stored in memory now

--- api/server.js
+++ api/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(session);
-
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -32,13 +30,6 @@ server.use(session({
   rolling: true,
   resave: false,
   saveUninitialized: false,
-  store: new KnexSessionStore({
-    knex: require('../data/db-config'),
-    tablename: 'sessions',
-    sidfieldname: 'sid',
-    createtable: true,
-    clearInterval: 1000 * 60 * 60,
-  }),
 }));
 server.use(cors());
 
